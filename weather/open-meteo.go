0b2acd1e750deb0b44ae75496552753d500f4d1e
Shawon <shawon.crosen@gmail.com>
2023-11-26 14:14:12 -0600

    weather stuff

--- weather/open-meteo.go
+++ weather/open-meteo.go
@@ -60,6 +60,31 @@ func NewForecastParams() ForecastParams {
 	return fp
 }
 
+func windDirection(w float64) string {
+	var dir string
+
+	switch {
+	case (w == 0):
+		dir = "N"
+	case (w > 0 && w < 90):
+		dir = "NNE"
+	case (w == 90):
+		dir = "E"
+	case (w > 90 && w < 180):
+		dir = "SSE"
+	case (w == 180):
+		dir = "S"
+	case (w > 180 && w < 270):
+		dir = "SSW"
+	case (w == 270):
+		dir = "W"
+	case (w > 270):
+		dir = "NNW"
+	}
+
+	return dir
+}
+
 func (w Weather) GetData() ForecastResponse {
 	target := ForecastResponse{}
 
@@ -111,15 +136,15 @@ func (w Weather) FormatData(fr ForecastResponse) Forecast {
 
 	for i := 0; i < 11; i++ {
 		h := HourlyForecast{}
-		h.Temp = fmt.Sprintf("%v\u0xB0", fr.HourlyData.Temperature[i])
-		h.ApparentTemp = fmt.Sprintf("%v%v", fr.HourlyData.ApparentTemperature[i], fr.HourlyUnits.ApparentTemperature[0])
-		h.CloudCover = fmt.Sprintf("%v%v", fr.HourlyData.CloudCover[i], fr.HourlyUnits.CloudCover[0])
-		h.Humidity = fmt.Sprintf("%v%v", fr.HourlyData.RelativeHumidity[i], fr.HourlyUnits.RelativeHumidity[0])
-		h.Precip = fmt.Sprintf("%v%v", fr.HourlyData.Precipitation[i], fr.HourlyUnits.Precipitation[0])
-		h.WeatherCode = fmt.Sprintf("%v%v", fr.HourlyData.WeatherCode[i], fr.HourlyUnits.WeatherCode[0])
-		h.WindDirection = fmt.Sprintf("%v%v", fr.HourlyData.WindDirection[i], fr.HourlyUnits.WindDirection[0])
-		h.WindGusts = fmt.Sprintf("%v%v", fr.HourlyData.WindGusts[i], fr.HourlyUnits.WindGusts[0])
-		h.WindSpeed = fmt.Sprintf("%v%v", fr.HourlyData.WindSpeed[i], fr.HourlyUnits.WindSpeed[0])
+		h.Temp = fmt.Sprintf("%v%v", fr.HourlyData.Temperature[i], fr.HourlyUnits.Temperature)
+		h.ApparentTemp = fmt.Sprintf("%v%v", fr.HourlyData.ApparentTemperature[i], fr.HourlyUnits.ApparentTemperature)
+		h.CloudCover = fmt.Sprintf("%v%v", fr.HourlyData.CloudCover[i], fr.HourlyUnits.CloudCover)
+		h.Humidity = fmt.Sprintf("%v%v", fr.HourlyData.RelativeHumidity[i], fr.HourlyUnits.RelativeHumidity)
+		h.Precip = fmt.Sprintf("%v %ves", fr.HourlyData.Precipitation[i], fr.HourlyUnits.Precipitation)
+		h.WeatherCode = fmt.Sprintf("%v", fr.HourlyData.WeatherCode[i])
+		h.WindDirection = fmt.Sprintf("%v", windDirection(fr.HourlyData.WindDirection[i]))
+		h.WindGusts = fmt.Sprintf("%v %v", fr.HourlyData.WindGusts[i], fr.HourlyUnits.WindGusts)
+		h.WindSpeed = fmt.Sprintf("%v %v", fr.HourlyData.WindSpeed[i], fr.HourlyUnits.WindSpeed)
 
 		f.Hourly = append(f.Hourly, h)
 		fmt.Println(h)
@@ -128,15 +153,15 @@ func (w Weather) FormatData(fr ForecastResponse) Forecast {
 	for j := 0; j < 7; j++ {
 		d := DailyForecast{}
 
-		d.TempMax = fmt.Sprintf("%v%v", fr.DailyData.TemperatureMax[j], fr.DailyUnits.TemperatureMax[0])
-		d.TempMin = fmt.Sprintf("%v%v", fr.DailyData.TemperatureMin[j], fr.DailyUnits.TemperatureMin[0])
-		d.PrecipProbability = fmt.Sprintf("%v%v", fr.DailyData.PrecipitationProbabilityMean[j], fr.DailyUnits.PrecipitationProbabilityMean[0])
-		d.PrecipTotal = fmt.Sprintf("%v%v", fr.DailyData.PrecipitationTotal[j], fr.DailyUnits.PrecipitationTotal[0])
-		d.Sunrise = fmt.Sprintf("%v%v", fr.DailyData.Sunrise[j], fr.DailyUnits.Sunrise[0])
-		d.Sunset = fmt.Sprintf("%v%v", fr.DailyData.Sunset[j], fr.DailyUnits.Sunset[0])
-		d.WeatherCode = fmt.Sprintf("%v%v", fr.DailyData.WeatherCode[j], fr.DailyUnits.WeatherCode[0])
-		d.WindGustsMax = fmt.Sprintf("%v%v", fr.DailyData.WindGustsMax[j], fr.DailyUnits.WindGustsMax[0])
-		d.WindSpeedMax = fmt.Sprintf("%v%v", fr.DailyData.WindSpeedMax[j], fr.DailyUnits.WindSpeedMax[0])
+		d.TempMax = fmt.Sprintf("%v%v", fr.DailyData.TemperatureMax[j], fr.DailyUnits.TemperatureMax)
+		d.TempMin = fmt.Sprintf("%v%v", fr.DailyData.TemperatureMin[j], fr.DailyUnits.TemperatureMin)
+		d.PrecipProbability = fmt.Sprintf("%v%v", fr.DailyData.PrecipitationProbabilityMean[j], fr.DailyUnits.PrecipitationProbabilityMean)
+		d.PrecipTotal = fmt.Sprintf("%v %ves", fr.DailyData.PrecipitationTotal[j], fr.DailyUnits.PrecipitationTotal)
+		d.Sunrise = fmt.Sprintf("%v", fr.DailyData.Sunrise[j])
+		d.Sunset = fmt.Sprintf("%v", fr.DailyData.Sunset[j])
+		d.WeatherCode = fmt.Sprintf("%v", fr.DailyData.WeatherCode[j])
+		d.WindGustsMax = fmt.Sprintf("%v %v", fr.DailyData.WindGustsMax[j], fr.DailyUnits.WindGustsMax)
+		d.WindSpeedMax = fmt.Sprintf("%v %v", fr.DailyData.WindSpeedMax[j], fr.DailyUnits.WindSpeedMax)
 
 		f.Daily = append(f.Daily, d)
 		fmt.Println(d)
